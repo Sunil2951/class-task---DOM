12c5b77222e28d1a0b678462331278a3652d423b
Sunil Joshi <167693023+Sunil2951@users.noreply.github.com>
2024-05-13 20:32:50 +0530

    to deploy in netlify

---
+++ script2.js
@@ -0,0 +1,16 @@
+function foo() {
+    var res = document.getElementById("firstname").value;
+    console.log(res);
+
+    var res1 = document.getElementById("middlename").value;
+    console.log(res1);
+
+    var res2 = document.getElementById("lastname").value;
+    console.log(res2);
+
+    var res3 = document.getElementById("email").value;
+    console.log(res3);
+
+    var res4 = document.getElementById("location").value;
+    console.log(res4);
+}
